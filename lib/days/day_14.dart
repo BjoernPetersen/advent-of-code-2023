ab81d75c1bbbf2a18bf650027725a7b8a55ec944
Björn Petersen <git@bjoernpetersen.net>
2023-12-15 13:42:36 +0100

    Slow part 2

--- lib/days/day_14.dart
+++ lib/days/day_14.dart
@@ -49,29 +49,67 @@ class Platform {
     return Platform._(result);
   }
 
-  Rock? operator[](Vector position) {
+  Rock? operator [](Vector position) {
     return _rocks[position.y][position.x];
   }
 
   bool contains(Vector position) {
-    return position.x >= 0 && position.x < width && position.y >= 0 && position.y < _rocks.length;
+    return position.x >= 0 &&
+        position.x < width &&
+        position.y >= 0 &&
+        position.y < _rocks.length;
   }
 
-  Platform tiltNorth() {
-    final direction = Vector.north;
-    final rocks = <List<Rock?>>[];
-    for (var y = 0; y < _rocks.length; y += 1) {
-      final row = <Rock?>[];
-      rocks.add(row);
-      for (var x = 0; x < width; x += 1) {
+  Platform tilt(Vector direction) {
+    final width = this.width;
+    final height = _rocks.length;
+    final List<List<Rock?>> rocks = List.generate(
+      height,
+      (index) => List.filled(width, null),
+      growable: false,
+    );
+
+    final int yStart;
+    final int yDirection;
+    final int yEnd;
+
+    if (direction.y < 0) {
+      yStart = 0;
+      yDirection = 1;
+      yEnd = height;
+    } else {
+      yStart = height - 1;
+      yDirection = -1;
+      yEnd = -1;
+    }
+
+    final int xStart;
+    final int xDirection;
+    final int xEnd;
+
+    if (direction.x < 0) {
+      xStart = 0;
+      xDirection = 1;
+      xEnd = width;
+    } else {
+      xStart = width - 1;
+      xDirection = -1;
+      xEnd = -1;
+    }
+
+    for (var y = yStart; y != yEnd; y += yDirection) {
+      final row = rocks[y];
+      for (var x = xStart; x != xEnd; x += xDirection) {
         final position = Vector(x: x, y: y);
         final rock = this[position];
-        row.add(rock);
+        row[x] = rock;
         if (rock?.isRound ?? false) {
           Vector blockerPosition;
-          for(blockerPosition = position + direction; contains(blockerPosition); blockerPosition += direction) {
+          for (blockerPosition = position + direction;
+              contains(blockerPosition);
+              blockerPosition += direction) {
             final existingRock = rocks[blockerPosition.y][blockerPosition.x];
-            if(existingRock != null) {
+            if (existingRock != null) {
               break;
             }
           }
@@ -110,10 +148,39 @@ final class PartOne implements IntPart {
   @override
   Future<int> calculate(Stream<String> input) async {
     final platform = await Platform.fromInput(input);
-    return platform.tiltNorth().totalLoad;
+    return platform.tilt(Vector.north).totalLoad;
+  }
+}
+
+@immutable
+final class PartTwo implements IntPart {
+  const PartTwo();
+
+  @override
+  Future<int> calculate(Stream<String> input) async {
+    var platform = await Platform.fromInput(input);
+
+    final cycleDirections = [
+      Vector.north,
+      Vector.west,
+      Vector.south,
+      Vector.east
+    ];
+
+    for (var i = 0; i < 1000000000; i += 1) {
+      if (i % 100000 == 0) {
+        print(i);
+      }
+      for (final direction in cycleDirections) {
+        platform = platform.tilt(direction);
+      }
+    }
+
+    return platform.totalLoad;
   }
 }
 
 const day = Day(
   PartOne(),
+  PartTwo(),
 );
